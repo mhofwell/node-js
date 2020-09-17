efd19fee3cc9c19387f5868d7899fad343cf277c
Michael Hofweller <michael.hofweller@gmail.com>
2020-09-17 12:22:22 -0700

    node

---
+++ node-school/learnyounode/baby-steps.js
@@ -0,0 +1,11 @@
+console.log(process.argv.length);
+
+const stringTotal = 5;
+
+console.log(Number(process.argv.length[3]) + Number(process.argv.length[4]));
+
+// for (let i = 0; i < process.argv.length; i + 1) {
+//         if (process.argv[i] > 1) {
+//                 stringTotal += Number(process.argv[i]);
+//         }
+// }
