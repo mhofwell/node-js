59a985f9f1a7c672d7f8cd3f4cce106c9263e241
Michael Hofweller <michael.hofweller@gmail.com>
2020-09-18 15:20:50 -0700

    node 3 done

--- node-school/learnyounode/my-first-io.js
+++ node-school/learnyounode/my-first-io.js
@@ -2,15 +2,6 @@ let count;
 const fs = require('fs');
 
 const buffer = fs.readFileSync(process.argv[2]);
-const buffString = buffer.toString().split('/\n/g');
-console.log(buffString);
+const buffString = buffer.toString().split('\n');
 
-// console.log(buffString.length);
-
-// buffer.forEach(entry => {
-//         if (entry === /\n/g) {
-//                 count += 1;
-//         }
-// });
-
-// console.log(count);
+console.log(buffString.length - 1);
