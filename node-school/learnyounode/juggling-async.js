8f28a752993cf7df8fa5cb72f14eb13df32a6e44
Michael Hofweller <michael.hofweller@gmail.com>
2020-09-28 14:59:19 -0700

    jugling async

--- node-school/learnyounode/juggling-async.js
+++ node-school/learnyounode/juggling-async.js
@@ -2,27 +2,49 @@ const http = require('http');
 
 // Create the http get requets for the data, assign them to a varible.
 
-function multiURL(url, url2, url3, url4) {
-        const urlArr = [url, url2, url3, url4];
+const urlArr = [process.argv[2], process.argv[3], process.argv[4], process.argv[5]];
+const promArr = [];
 
-        let strData = '';
+// for (let i = 2; i < process.argv.length; i + 1) {
+//         urlArr.push(process.argv[i]);
+// }
 
-        urlArr.forEach(addy => {
-                http.get(addy, response => {
-                        response.on('error', console.error);
+// console.log(urlArr);
 
-                        response.on('data', data => {
-                                strData = data.toString();
-                                return strData;
-                        });
+urlArr.forEach(addy => {
+        promArr.push(
+                new Promise(resolve => {
+                        resolve(
+                                http
+                                        .get(addy, response => {
+                                                let strData = '';
 
-                        response.on('end', () => {
-                                console.log(strData);
-                        });
-                });
-        });
-}
+                                                response.on('error', err => {
+                                                        console.error(err);
+                                                });
 
-multiURL(process.argv[2], process.argv[3], process.argv[4], process.argv[5]);
+                                                response.on('data', data => {
+                                                        strData += data.toString();
+                                                });
+
+                                                response.on('end', () => {
+                                                        console.log(strData);
+                                                });
+                                        })
+                                        .on('error', console.error)
+                        );
+                })
+        );
+});
+
+// const allRes = Promise.all(promArr);
+
+// allRes.then(responses => console.log(`${responses}`));
+
+// for (let i = 0; i < process.argv.length; i + 1) {
+//         console.log(Object.values(allRes[i]));
+// }
+
+// allRes.then(resolved => console.log(resolved));
 
 // resolve the http requests with Promise.all() to ensure they all resolve before logging them.
