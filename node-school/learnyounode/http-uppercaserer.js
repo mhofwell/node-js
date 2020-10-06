906293b2335ed1a5236bcb194f820329369f770a
Michael Hofweller <michael.hofweller@gmail.com>
2020-10-06 14:58:28 -0700

    did it

---
+++ node-school/learnyounode/http-uppercaserer.js
@@ -0,0 +1,24 @@
+const http = require('http');
+
+const port = process.argv[2];
+
+function handleRequest(req, res) {
+        req.on('error', err => console.error(err));
+        let body = '';
+        if (req.method === 'POST') {
+                req.setEncoding('utf8').on('data', chunk => {
+                        body += chunk;
+                });
+        } else {
+                console.log('Cannot accept GET request!');
+        }
+        req.on('end', () => {
+                res.write(body.toUpperCase());
+                res.end();
+        });
+}
+
+const server = http.createServer(handleRequest);
+
+server.listen(port);
+console.log(`Listening on port ${port}`);
