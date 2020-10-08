b3c3e49dbb17408ac373cf867aca8faf78999f1e
Michael Hofweller <michael.hofweller@gmail.com>
2020-10-08 10:31:51 -0700

    mongo-update-entries exercise

--- node-school/learnyoumongo/mongo-update-entries.js
+++ node-school/learnyoumongo/mongo-update-entries.js
@@ -1,22 +1,29 @@
 const { MongoClient } = require('mongodb');
 
+// function to handle the db query
+
 function handleQuery(err, db) {
         if (err) throw err;
         db.db()
                 .collection('users')
                 .update(
+                        // search by unique id
                         { username: 'tinatime' },
                         {
+                                // change the age to 40
                                 $set: {
                                         age: 40,
                                 },
                         },
+                        // handle the error and log the response object to the console.
                         (err, res) => {
                                 if (err) throw err;
-                                console.log(JSON.stringify(res));
+                                console.log(res.toString());
                                 db.close();
                         }
                 );
 }
+
+// connect the MongoDB
 
 MongoClient.connect(`mongodb://localhost:27017/${process.argv[2]}`, handleQuery);
